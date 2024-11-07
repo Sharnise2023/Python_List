00b483c7f98b9141d0387718d614b626b5e1dcc5
Sharnise Nalepa <nalepa96@gmail.com>
2024-11-07 11:59:51 -0500

    first commit

---
+++ 1. Python List Transformation.py
@@ -0,0 +1,10 @@
+
+# Task 1:
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
+
+# Task 2:
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+average_grade = sum(grades) / len(grades)
+print("Average grade", average_grade)
---
+++ 2. Advanced List Methods and Identity Operators.py
@@ -0,0 +1,7 @@
+# Task 1:
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice submitted their and assignment and attended class.")
+else:
+    print("Alice did not submit the assignment or attend class.")
